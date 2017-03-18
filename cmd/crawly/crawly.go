628f86b0e86baad48ecc460ae7e98b818b764e1d
Denbeigh Stevens <denbeigh@uber.com>
2017-03-18 12:48:56 -0700

    Cleaned up main package

--- cmd/crawly/crawly.go
+++ cmd/crawly/crawly.go
@@ -3,11 +3,49 @@ package main
 import (
 	"github.com/denbeigh2000/crawly"
 
+	"flag"
 	"fmt"
 	"log"
 )
 
 type FakeCrawler map[string][]string
+
+var (
+	concurrency = flag.Int("concurrency", 1, "Number of HTTP Fetching routines to intstantiate")
+	start       = flag.String("start-at", "http://www.somecompany.com", "The URL to start crawling from")
+)
+
+var defaultCrawler = FakeCrawler{
+	"http://www.somecompany.com": []string{
+		"http://www.somecompany.com",
+		"http://www.somecompany.com/about",
+		"http://www.somecompany.com/jobs",
+		"http://www.somecompany.com/login",
+	},
+	"http://www.somecompany.com/about": []string{
+		"http://www.somecompany.com",
+		"http://www.somecompany.com/jobs",
+		"http://www.somecompany.com/foo",
+	},
+	"http://www.somecompany.com/jobs": []string{
+		"http://www.somecompany.com",
+	},
+	"http://www.somecompany.com/foo": []string{
+		"http://www.somecompany.com",
+		"http://www.somecompany.com/login",
+		"http://www.somecompany.com/bar",
+		"http://www.somecompany.com/baz",
+	},
+	"http://www.somecompany.com/bar": []string{
+		"http://www.somecompany.com/bar",
+	},
+	"http://www.somecompany.com/baz": []string{
+		"http://www.somecompany.com/bar",
+	},
+	"http://www.somecompany.com/login": []string{
+		"http://www.somecompany.com/bar",
+	},
+}
 
 func (f FakeCrawler) Crawl(url string) ([]string, error) {
 	if v, ok := f[url]; ok {
@@ -17,39 +55,19 @@ func (f FakeCrawler) Crawl(url string) ([]string, error) {
 	}
 }
 
+func init() {
+	flag.Parse()
+}
+
 func main() {
-	crawler := FakeCrawler{
-		"http://www.somecompany.com": []string{
-			"http://www.somecompany.com",
-			"http://www.somecompany.com/about",
-			"http://www.somecompany.com/jobs",
-			"http://www.somecompany.com/login",
-		},
-		"http://www.somecompany.com/about": []string{
-			"http://www.somecompany.com",
-			"http://www.somecompany.com/jobs",
-			"http://www.somecompany.com/foo",
-		},
-		"http://www.somecompany.com/jobs": []string{
-			"http://www.somecompany.com",
-		},
-		"http://www.somecompany.com/foo": []string{
-			"http://www.somecompany.com",
-			"http://www.somecompany.com/login",
-			"http://www.somecompany.com/bar",
-			"http://www.somecompany.com/baz",
-		},
-		"http://www.somecompany.com/bar": []string{
-			"http://www.somecompany.com/bar",
-		},
-		"http://www.somecompany.com/baz": []string{
-			"http://www.somecompany.com/bar",
-		},
-		"http://www.somecompany.com/login": []string{
-			"http://www.somecompany.com/bar",
-		},
+	if *concurrency <= 0 {
+		log.Panic("Concurrency must be at least 1, got %v", *concurrency)
 	}
-	c := crawly.NewCrawly("http://www.somecompany.com", crawler, 1)
+	if *start == "" {
+		log.Panic("Must ptrovide a start URL", *concurrency)
+	}
+
+	c := crawly.NewCrawly("http://www.somecompany.com", defaultCrawler, *concurrency)
 
 	urls := c.URLs()
 	for url := range urls {
